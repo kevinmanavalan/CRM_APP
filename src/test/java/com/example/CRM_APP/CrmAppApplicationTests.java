40add379dcc4a7410e654cb0b0026c54092e9100
kevinmanavalan <kevinseby20@gmail.com>
2024-07-17 14:32:33 +0530

    base app config

---
+++ src/test/java/com/example/CRM_APP/CrmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRM_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
